1753d728bf23ae37b1c5e8bc33bdc64b42d1770c
Ed Jones <ed@errorstudio.co.uk>
2014-04-30 13:14:28 +0100

    A refactor of the SMS functionality into a subclass. This should be the way to go for all the functions - into subclasses of VoipfoneClient::Client.
    Bumped gem version to 0.1.0 because this is quite a big change.

--- lib/voipfone_client/sms.rb
+++ lib/voipfone_client/sms.rb
@@ -1,25 +1,29 @@
-class VoipfoneClient::Client
-  # Send an sms from your account.
-  # @param to [String] the phone number to send the SMS to, as a string. Spaces will be stripped; + symbol allowed.
-  # @param from [String] the phone number to send the SMS from, as a string. Spaces will be stripped; + symbol allowed.
-  # @param message [String] the message to send. The first 160 characters only will be sent.
-  def send_sms(to:to, from:from, message:message)
-    if to.nil? || from.nil? || message.nil?
-      raise ArgumentError, "You need to include 'to' and 'from' numbers and a message to send an SMS"
-    end
-    to = to.gsub(" ","")
-    from = from.gsub(" ","")
-    parameters = {
-      "sms-send-to" => to,
-      "sms-send-from" => from,
-      "sms-message" => message
-    }
-    request = @browser.post("#{VoipfoneClient::API_POST_URL}?smsSend", parameters)
-    response = parse_response(request)
-    if response == "ok"
-      return true
-    else
-      raise VoipfoneAPIError, response
+module VoipfoneClient
+  class SMS < Client
+    attr_accessor :from, :to, :message
+
+    # Send an sms from your account.
+    # @param to [String] the phone number to send the SMS to, as a string. Spaces will be stripped; + symbol allowed.
+    # @param from [String] the phone number to send the SMS from, as a string. Spaces will be stripped; + symbol allowed.
+    # @param message [String] the message to send. The first 160 characters only will be sent.
+    def send
+      if @to.nil? || @from.nil? || @message.nil?
+        raise ArgumentError, "You need to include 'to' and 'from' numbers and a message to send an SMS"
+      end
+      to = @to.gsub(" ","")
+      from = @from.gsub(" ","")
+      parameters = {
+        "sms-send-to" => to,
+        "sms-send-from" => from,
+        "sms-message" => @message[0..159]
+      }
+      request = @browser.post("#{VoipfoneClient::API_POST_URL}?smsSend", parameters)
+      response = parse_response(request)
+      if response == "ok"
+        return true
+      else
+        raise VoipfoneAPIError, response
+      end
     end
   end
 end
