7bb8efa2aa555c627288b8725176bfa35ee92f94
Ed Jones <ed@errorstudio.co.uk>
2014-05-11 11:11:42 +0100

    Return a list of RegisteredMobile objects

---
+++ lib/voipfone_client/registered_mobile.rb
@@ -0,0 +1,18 @@
+module VoipfoneClient
+	class RegisteredMobile < Client
+    attr_accessor :number, :name
+
+    class << self
+      def all
+        r = RegisteredMobile.new
+        request = r.browser.get("#{VoipfoneClient::API_GET_URL}?registeredMobile")
+        r.parse_response(request)["registeredMobile"].collect do |m|
+          mobile = RegisteredMobile.new
+          mobile.number = m[0]
+          mobile.name = m[1]
+          mobile
+        end
+      end
+    end
+	end
+end
