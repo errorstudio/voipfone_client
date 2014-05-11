99b6e1af0cd3c9d12441a7964dc3aec94299baa6
Ed Jones <ed@errorstudio.co.uk>
2014-05-11 11:48:22 +0100

    Added constructor for SMS

--- lib/voipfone_client/sms.rb
+++ lib/voipfone_client/sms.rb
@@ -2,10 +2,18 @@ module VoipfoneClient
   class SMS < Client
     attr_accessor :from, :to, :message
 
-    # Send an sms from your account.
+    # Constructor to create an SMS - optionally pass in to, from and message
     # @param to [String] the phone number to send the SMS to, as a string. Spaces will be stripped; + symbol allowed.
     # @param from [String] the phone number to send the SMS from, as a string. Spaces will be stripped; + symbol allowed.
     # @param message [String] the message to send. The first 160 characters only will be sent.
+    def initialize(to: nil, from: nil, message: nil)
+      @to = to
+      @from = from
+      @message = message
+      super()
+    end
+
+    # Send an sms from your account.
     def send
       if @to.nil? || @from.nil? || @message.nil?
         raise ArgumentError, "You need to include 'to' and 'from' numbers and a message to send an SMS"
